26a603313be5d9422a004ac46cb86f0611d7d1d2
Poom Yimyuean <lebrancconvas@gmail.com>
2021-06-03 00:22:20 +0700

    :tada: Project Init.

---
+++ client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <div>{{pokemonname}}</div>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+import Axios from 'axios'
+export default {
+  name: 'Home',
+  setup() {
+    let pokemonname = ref("")
+    Axios.get('https://pokeapi.co/api/v2/pokemon/ditto').then(res => {
+      pokemonname = res.data.form[0] 
+    })
+    return {pokemonname}
+  }
+}
+</script>
